fe9fd8f7264c30b28a75532fbdc5b586a68d43a0
frabottt <100599596+frabottt@users.noreply.github.com>
2022-05-18 07:08:19 -0400

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,67 @@
+#defina una funcion en python que acepte el radio y devuelva el valor del area de un circulo de esas dimensiones
+
+def calcular(r,pi):
+    a = pi * r**2
+    return (a)
+
+from math import pi
+
+r = float(input("digite el numero para el radio:  "))
+a = pi * r**2
+
+print("el resultado es igual a: ",a)
+
+
+#defina una funcion en python que acepte 3 valores y devuelva solo el maximo de los tres
+
+def main():
+    if (a > b) and (a > c): {
+        
+        print("el numero mayor es:",a)
+    }
+    
+    if (b > a) and (b > c): {
+        print("el numero mayor es:"b)
+    }
+
+    if(c > a) and (c > a): {
+        print("el numero mayor es c")
+    }
+    main()
+
+    a = float(input("dgite un numero:"))
+    b = float(input("digite un numero"))
+    c = float(input("digite un numero"))
+
+    if (a > b) and (a > c): {
+        print("el numero mayor es:,a")
+    }
+    
+    if (b > a) and (b > c): {
+        print("el numero mayor es:",b)
+     
+    }
+
+    if (c > a) and (c > a): {
+        print("el numero mayor es:",c)
+    }
+
+
+    #dado a la lista de enteros, defina una funcion en python que devuelva la suma de solo los valores impares de dicha lista
+
+    numeros = [1,2,3,4,5,6,7,8,9,10]
+    suma_impares =0
+    i=0
+
+    def main(numeros):
+
+        while i <1en(numeros):
+            if numeros[i] % 2 =0
+            suma_impares += numeros[1]
+
+            1 +=1
+        print("suma de valores impares en el rango 1 a 10:",suma_impares)  
+
+        main(suma_impares)  
+
+    
